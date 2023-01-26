e8f3437aa940a5d42cb7662f94a5fbadee9cc217
Bryan Lai <bryanlais@gmail.com>
2023-01-26 23:54:24 +0800

    Add pacman logs, firefox user.js & more

---
+++ .mozilla/firefox/User Mods/user.js
@@ -0,0 +1,13 @@
+user_pref("ui.prefersReducedMotion", 0);
+user_pref("browser.download.autohideButton", false);
+user_pref("browser.download.useDownloadDir", true);
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("services.sync.prefs.sync.browser.uiCustomization.state", true);
+user_pref("dom.security.https_first", true);
+
+// //// <https://wiki.archlinux.org/title/Firefox/Tweaks#Jerky_or_choppy_scrolling>
+// user_pref("mousewheel.min_line_scroll_amount", 40);
+// user_pref("general.smoothScroll", false);
+// user_pref("general.smoothScroll.pages", false);
+// user_pref("image.mem.min_discard_timeout_ms", 2100000000);
+// user_pref("image.mem.max_decoded_image_kb", 512);
--- apps/utils/btrfs-snapshot.sh
+++ apps/utils/btrfs-snapshot.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 # btrfs snapshot setup
 
-set -xe  # echo & quit on error
+set -x  # echo
+set -e  # quit on error
 name=$1
+
 MNT_BTRSYSTEM=/run/media/root/btrsystem
-[[ -z $name ]] && exit 1
+
+ls -alF --color=always /.snapshots
+if [[ -z $name ]]; then
+    # shellcheck disable=SC2034
+    ERROR="a name for the snapshot is required"
+    exit 1
+fi
 
 sudo mount --mkdir "$MNT_BTRSYSTEM"
 
 # sudo --reset-timestamp: ask for password confirmation
 for subvol in root home; do
-    # readonly
-    sudo --reset-timestamp \
-        btrfs subvolume snapshot -r "$MNT_BTRSYSTEM"/{@"$subvol",@snapshots/"$subvol"-"$name"-ro}
-
     # readwrite
     sudo --reset-timestamp \
-        btrfs subvolume snapshot "$MNT_BTRSYSTEM"/{@"$subvol",@snapshots/"$subvol"-"$name"-rw}
+        btrfs subvolume snapshot "$MNT_BTRSYSTEM"/{@"$subvol",@snapshots/"$subvol"-"$name"}
+        # `-r` flag for readonly
 done
+
+# cleanup
+target="$MNT_BTRSYSTEM"/@snapshots/root-"$name"
+{
+    cd "$target" || exit 1
+    sudo --reset-timestamp /bin/rm -rI "$target"/opt/{anaconda,Mathematica}
+}
